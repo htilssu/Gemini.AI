dc90acc15fc51511e6c9638fa76380cf7b376b10
htilssu <tolashuu@gmail.com>
2025-10-13 15:05:48 +0700

    Initial

---
+++ src/Gemini.Agent.AI/GeminiChatClient.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.AI;
+
+namespace Gemini.Agent.AI;
+
+public class GeminiChatClient : IChatClient
+{
+    private HttpClient client = null!;
+    private string _modelName = "gemini-2.5-flash-lite";
+
+    public GeminiChatClient()
+    {
+        client.BaseAddress = new Uri("");
+    }
+
+    public void Dispose()
+    {
+        client.Dispose();
+    }
+
+    public async Task<ChatResponse> GetResponseAsync(IEnumerable<ChatMessage> messages,
+        ChatOptions? options = null,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        client = new HttpClient();
+        var response = await client.GetAsync(new Uri(
+                $"https://generativelanguage.googleapis.com/v1beta/models/{_modelName}:generateContent"),
+            cancellationToken);
+
+        return new ChatResponse(new ChatMessage(ChatRole.Assistant,
+            await response.Content.ReadAsStringAsync(cancellationToken)));
+    }
+
+    public async IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(
+        IEnumerable<ChatMessage> messages,
+        ChatOptions? options = null,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        client = new HttpClient();
+        var response = await client.GetAsync(new Uri(
+                $"https://generativelanguage.googleapis.com/v1beta/models/{_modelName}:streamGenerateContent"),
+            cancellationToken);
+
+        return new ChatResponse(new ChatMessage(ChatRole.Assistant,
+            await response.Content.ReadAsStringAsync(cancellationToken)));
+    }
+
+    public object? GetService(Type serviceType, object? serviceKey = null)
+    {
+        throw new NotImplementedException();
+    }
+}
