3aae15128a17e76d993e808ed8500420b3377266
htilssu <tolashuu@gmail.com>
2025-10-14 22:00:29 +0700

    feat: add initial implementation of ChatClient and GeminiChatClient with basic response handling

---
+++ src/Gemini.Agent.AI/ChatClient.cs
@@ -0,0 +1,15 @@
+﻿using System.ClientModel;
+
+namespace Gemini.Agent.AI;
+
+public class ChatClient
+{
+    public ChatClient(string model, string apiKey) : this(model, new ApiKeyCredential(apiKey),
+        new GeminiClientOptions())
+    {
+    }
+
+    private ChatClient(string model, ApiKeyCredential apiKey, GeminiClientOptions geminiClientOptions)
+    {
+    }
+}
---
+++ src/Gemini.Agent.AI/GeminiClientOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ClientModel.Primitives;
+
+namespace Gemini.Agent.AI;
+
+public class GeminiClientOptions : ClientPipelineOptions
+{
+    private Uri _endpoint;
+    
+    public Uri Endpoint
+    {
+        get => _endpoint;
+        set
+        {
+            AssertNotFrozen();
+            _endpoint = value;
+        }
+    }
+}
